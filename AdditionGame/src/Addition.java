851ea2d9770ce37af24a5a437fb10b7aaf0d90cc
Joe Rymer <rymerj@student.swosu.edu>
2015-10-14 10:29:10 -0500

    first commit

---
+++ AdditionGame/src/Addition.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Addition {
+
+	public static void main(String[] args) {
+	//Generate random numbers
+		int numb1 = (int)(Math.random() * 10);
+		int numb2 = (int)(Math.random() * 10);
+	}
+
+}
